0022522e026948f1fb89ab925d9f66cbb41e1317
FERN <rxmm34@durham.ac.uk>
2021-10-27 10:33:10 +0100

    Added functionality to search bar.

--- searchResources.js
+++ searchResources.js
@@ -1,8 +1,28 @@
 const btn = document.getElementById("resourceSearchButton");
 const search = document.getElementById("resourceSearch");
-let results = [["natural gas", "https://www.youtube.com/watch?v=-njmj0diWu8"], ["oil", "placeholder"]];
+let results = [["natural gas", "video", "https://www.youtube.com/embed/-njmj0diWu8", "Annual emissions:"], 
+               ["oil", "text", "placeholder"]];
+
+function onSearch(txt) {
+    let unfound = 1;
+    for (let i=0; i < results.length; i+=1) {
+        if (results[i][0] == txt.toLowerCase()) {
+            alert("Match found")
+            unfound = 0;
+            if (results[i][1] == "video") {
+                document.getElementById("result").innerHTML = "<iframe id='naturalGasVideo' width='800' height='550' src='" + results[i][2] + "'></iframe>" + "<br>" + results[i][3]; 
+            } if (results[i][1] == "text") {
+                document.getElementById("result").innerHTML = results[i][2];
+            }
+            break;
+        }
+    }
+    if (unfound == 1) {
+        document.getElementById("result").innerText = "No result found";
+    }
+}
 
 btn.addEventListener('click', () => {
     const userIn = search.value;
-    alert(userIn);
+    onSearch(userIn);
 });

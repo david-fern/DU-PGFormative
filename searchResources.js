dadde6feae7bfb076ea57b8986283aec24ed1370
FERN <rxmm34@durham.ac.uk>
2021-10-27 10:47:16 +0100

    Cleaning up page and adding resources.

--- searchResources.js
+++ searchResources.js
@@ -1,13 +1,13 @@
 const btn = document.getElementById("resourceSearchButton");
 const search = document.getElementById("resourceSearch");
-let results = [["natural gas", "video", "https://www.youtube.com/embed/-njmj0diWu8", "Annual emissions:"], 
-               ["oil", "text", "placeholder"]];
+let results = [["natural gas", "video", "https://www.youtube.com/embed/-njmj0diWu8", ""], 
+            ["oil", "text", "placeholder"],
+            ["coal", "video", "https://www.youtube.com/embed/3lngKyS8gVI", "Coal is on the decline, but it is still the most widely used fuel."]];
 
 function onSearch(txt) {
     let unfound = 1;
     for (let i=0; i < results.length; i+=1) {
         if (results[i][0] == txt.toLowerCase()) {
-            alert("Match found")
             unfound = 0;
             if (results[i][1] == "video") {
                 document.getElementById("result").innerHTML = "<iframe id='naturalGasVideo' width='800' height='550' src='" + results[i][2] + "'></iframe>" + "<br>" + results[i][3]; 
